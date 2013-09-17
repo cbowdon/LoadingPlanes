e8616ba2bd1319ee34f600eba46ee88786c839d1
Chris Bowdon <c.bowdon@gmail.com>
2013-09-17 03:16:35 +0100

    Beginning refactoring, added Haddock markup.

--- GNUmakefile
+++ GNUmakefile
@@ -1,14 +1,19 @@
 SOURCES=Main.hs
 EXE=bin/LoadingPlanes
 
-all: $(SOURCES)
+all: compile doc lint
+
+compile: $(SOURCES)
 	ghc --make -Wall $^ -outputdir bin -o $(EXE)
 
 run: all
 	$(EXE)
 
 lint:
-	hlint *.hs
+	hlint $(SOURCES)
+
+doc:
+	haddock -h $(SOURCES)
 
 clean:
 	rm -f bin/*.hi bin/*.o $(EXE)
--- Main.hs
+++ Main.hs
@@ -1,48 +1,69 @@
+{- |
+Loading Planes Simulation
+
+The great quest to master Haskell/tackle interesting problems continues. I vaguely remember seeing a news article about simulations showing the most efficient way to load passengers onto a plane. As I recall, letting people on at random is least efficient, loading back-to-front somewhat efficient and loading windows-to-aisles most efficient. This project is a recreation of that simulation. The passengers will step in turn towards their seat and time will be the total number of steps required.
+
+-}
 module Main where
 
-import Data.Word
+import Data.Word (Word)
+import qualified Data.Set as Set
+import qualified Data.Map as Map
 import Test.HUnit hiding (Node, path)
 
-data Node = Node Int Int Word
+-- * Types
+-- | An (x,y) point that can be occupied.
+data Node = Node Word Word deriving (Eq, Ord, Show)
 
-instance Show Node where
-    show (Node x y n) = show (x,y,n)
-
-instance Eq Node where
-    Node x y _ == Node x' y' _ = (x,y) == (x',y')
-
-instance Ord Node where
-    compare (Node _ _ n) (Node _ _ n') = compare n n'
+-- * Test constants
+origin :: Node
+origin = Node 0 0
 
 target :: Node
-target = Node 3 8 0
+target = Node 3 8
 
-origin :: Node
-origin = Node 0 0 0
+-- | The natural L-shaped path that would be taken if no obstructions were present.
+lPath :: [Node]
+lPath = [Node 0 0, Node 1 0, Node 2 0, Node 3 0, Node 3 1, Node 3 2, Node 3 3, Node 3 4, Node 3 5, Node 3 6, Node 3 7, Node 3 8]
 
-path :: [Node]
-path = [Node 0 0 0, Node 1 0 10, Node 2 0 9, Node 3 0 8, Node 3 1 7, Node 3 2 6, Node 3 3 5, Node 3 4 4, Node 3 5 3, Node 3 6 2, Node 3 7 1, Node 3 8 0]
+-- | A set of obstructions.
+occupiedNodes :: Set.Set Node
+occupiedNodes = Set.fromList [Node 0 7, Node 1 0, Node 1 1, Node 1 2, Node 1 3, Node 1 4, Node 1 7, Node 2 6]
 
+-- | The optimal path around the obstructions.
+mPath :: [Node]
+mPath = [Node 0 0, Node 0 1, Node 0 2, Node 0 3, Node 0 4, Node 0 5, Node 1 5, Node 2 5, Node 3 5, Node 3 6, Node 3 7, Node 3 8]
+
+-- * Tests
 tests :: Test
-tests = test [  TestCase (assertEqual "Self explored" False (unexplored [target] target)),
-                TestCase (assertEqual "Self explored n+1" False (unexplored [target] (Node 3 8 1))),
-                TestCase (assertEqual "Self explored multiple" False (unexplored [target, Node 3 7 0, Node 3 1 3] (Node 3 8 1))),
-                TestCase (assertEqual "Explored but better" True (unexplored [Node 3 8 1] target)),
-                TestCase (assertEqual "Unexplored" True (unexplored [Node 3 7 1] target)),
-                TestCase (assertEqual "To origin" 90 (length $ findPaths origin target)),
-                TestCase (assertEqual "To origin" origin (head $ findPaths origin target)),
-                TestCase (assertEqual "Adjacent in set" [Node 1 0 10, Node 0 1 10] (adjacentInSet [Node 0 0 11, Node 1 0 10, Node 0 1 10, Node 2 0 9] (Node 0 0 11))),
-                TestCase (assertEqual "Simple path" path (minPath origin target)) ]
-
+tests = test [  TestCase (assertEqual "Simple path" lPath (minPath Set.empty origin target)),
+                TestCase (assertEqual "Simple path" mPath (minPath occupiedNodes origin target)) ]
+-- * Functions
 main :: IO ()
-main = do
-    c <- runTestTT tests
-    print c
-    let k = findPaths origin target
-    print k
-    print $ length k
-    print $ minPath origin target
+main = print "Hello world"
 
+-- | Finds a minimum path around the obstructions from src to dest.
+minPath :: Set.Set Node     -- ^ Set of obstructed nodes
+        -> Node             -- ^ Source node
+        -> Node             -- ^ Destination node
+        -> [Node]           -- ^ Minimum path
+minPath = undefined
+
+-- | Predicate for whether a node is in the 10x10 world or not.
+inWorld :: Node -> Bool
+inWorld (Node x y)
+    | x > 9     = False
+    | y > 9     = False
+    | otherwise = True
+
+-- | Make graph of nodes connecting src to dest, with weights corresponding to distance from dest.
+makeGraph   :: Set.Set Node         -- ^ Set of obstructed nodes
+            -> Node                 -- ^ Source node
+            -> Node                 -- ^ Destination node
+            -> Map.Map Node Word    -- ^ Weighted graph of nodes
+makeGraph = undefined
+
+{-
 -- walk by moving to adjacent cell with lowest count
 minPath :: Node -> Node -> [Node]
 minPath src dest =
@@ -70,7 +91,7 @@ nodeClear (Node x y _)
     | otherwise = True
 
 adjacent :: Node -> [Node]
-adjacent (Node x y n)  = filter nodeClear [Node (x-1) y (n+1), Node (x+1) y (n+1), Node x (y-1) (n+1), Node x (y+1) (n+1)]
+adjacent (Node x y)  = filter nodeClear [Node (x-1) y, Node (x+1) y, Node x (y-1), Node x (y+1)]
 
 findPaths :: Node -> Node -> [Node]
 findPaths src dest = rec src [dest] []
@@ -91,3 +112,4 @@ unexplored explored that = foldl (\b a -> b && diffOrBetter that a) True explore
 
 diffOrBetter :: Node -> Node -> Bool
 diffOrBetter (Node x y n) (Node x' y' n') = x /= x' || y /= y' || x == x' && y == y' && n < n'
+-}
